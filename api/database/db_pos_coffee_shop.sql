a78c53282609626374e1ec7a5814f5da54e9ee79
Devan Firmansyah Ramadhan <devanramadhan92@gmail.com>
2021-12-28 11:40:01 +0700

    add

---
+++ api/database/db_pos_coffee_shop.sql
@@ -0,0 +1,274 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 28 Des 2021 pada 05.39
+-- Versi server: 10.4.19-MariaDB
+-- Versi PHP: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pos_coffee_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `name`) VALUES
+(1, 'admin', '$2b$10$m.b46brXGqwpeCB4DP7CTOx3C1scPiqSrEvN.ARjSOACwd1CSoO0i', 'Admin Coffee Shop');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bill`
+--
+
+CREATE TABLE `bill` (
+  `id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `bill` varchar(50) NOT NULL,
+  `customer_name` varchar(50) NOT NULL,
+  `total_price` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `bill`
+--
+
+INSERT INTO `bill` (`id`, `admin_id`, `bill`, `customer_name`, `total_price`, `created_at`) VALUES
+(1, 1, 'BILL-0001', 'Devan', 64000, '2021-12-28 04:35:13');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bill_detail`
+--
+
+CREATE TABLE `bill_detail` (
+  `id` int(11) NOT NULL,
+  `bill_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `total_price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `bill_detail`
+--
+
+INSERT INTO `bill_detail` (`id`, `bill_id`, `item_id`, `qty`, `price`, `total_price`) VALUES
+(1, 1, 2, 1, 20000, 20000),
+(2, 1, 3, 2, 22000, 44000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `item`
+--
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL,
+  `item_category_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `image` varchar(256) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `stock` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `item`
+--
+
+INSERT INTO `item` (`id`, `item_category_id`, `name`, `image`, `description`, `stock`, `price`, `is_active`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Espresso', '1640398801768.jpeg', 'Single Shot', 30, 12000, 1, '2021-12-13 16:30:20', '2021-12-25 03:15:36'),
+(2, 1, 'Caramel Macchiato (Hot)', '1640398649311.jpeg', 'Single Ristretto with Fresh Milk and Palm Sugar', 22, 20000, 1, '2021-12-13 16:30:49', '2021-12-28 04:35:13'),
+(3, 1, 'Cappucino (Hot)', '1640398850526.jpeg', 'Double Ristretto, Fresh Milk', 21, 22000, 1, '2021-12-13 16:31:19', '2021-12-28 04:35:13'),
+(4, 1, 'Esprecoal (Ice)', '1640398894152.jpeg', 'Freshly Brewed Espresso With Activated Charcoal And Fresh Milk', 36, 25000, 1, '2021-12-13 16:31:42', '2021-12-25 10:23:24'),
+(12, 2, 'Taro Ice', '1640402725769.jpeg', 'Premium Taro Powder With Fresh Milk', 32, 22000, 1, '2021-12-25 03:13:54', '2021-12-25 10:23:24'),
+(13, 2, 'Red Velvet (Hot)', '1640402720531.jpeg', 'Premium Red Velvet with Fresh Milk', 28, 20000, 1, '2021-12-25 03:14:59', '2021-12-25 10:23:24');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `item_category`
+--
+
+CREATE TABLE `item_category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `item_category`
+--
+
+INSERT INTO `item_category` (`id`, `name`, `is_active`) VALUES
+(1, 'Coffee', 1),
+(2, 'Non Coffee', 1),
+(3, 'Tea Series', 1),
+(4, 'Fruits', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `admin_id` (`admin_id`);
+
+--
+-- Indeks untuk tabel `bill_detail`
+--
+ALTER TABLE `bill_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bill_id` (`bill_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indeks untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `admin_id` (`admin_id`);
+
+--
+-- Indeks untuk tabel `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `item_category_id` (`item_category_id`);
+
+--
+-- Indeks untuk tabel `item_category`
+--
+ALTER TABLE `item_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `bill_detail`
+--
+ALTER TABLE `bill_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT untuk tabel `item`
+--
+ALTER TABLE `item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `item_category`
+--
+ALTER TABLE `item_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `bill`
+--
+ALTER TABLE `bill`
+  ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `bill_detail`
+--
+ALTER TABLE `bill_detail`
+  ADD CONSTRAINT `bill_detail_ibfk_1` FOREIGN KEY (`bill_id`) REFERENCES `bill` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bill_detail_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`item_category_id`) REFERENCES `item_category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
