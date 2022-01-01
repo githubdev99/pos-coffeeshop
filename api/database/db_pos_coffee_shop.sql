ef733a07d4185b19945cca2d05deb299166fd2c1
Devan Firmansyah Ramadhan <devanramadhan92@gmail.com>
2022-01-01 17:35:54 +0700

    add

--- api/database/db_pos_coffee_shop.sql
+++ api/database/db_pos_coffee_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Des 2021 pada 05.39
+-- Waktu pembuatan: 01 Jan 2022 pada 11.34
 -- Versi server: 10.4.19-MariaDB
 -- Versi PHP: 8.0.7
 
@@ -61,7 +61,8 @@ CREATE TABLE `bill` (
 --
 
 INSERT INTO `bill` (`id`, `admin_id`, `bill`, `customer_name`, `total_price`, `created_at`) VALUES
-(1, 1, 'BILL-0001', 'Devan', 64000, '2021-12-28 04:35:13');
+(1, 1, 'BILL-0001', 'Devan', 64000, '2021-12-28 04:35:13'),
+(2, 1, 'BILL-0002', 'Devan Ramadhan', 70000, '2021-12-28 06:56:38');
 
 -- --------------------------------------------------------
 
@@ -84,7 +85,9 @@ CREATE TABLE `bill_detail` (
 
 INSERT INTO `bill_detail` (`id`, `bill_id`, `item_id`, `qty`, `price`, `total_price`) VALUES
 (1, 1, 2, 1, 20000, 20000),
-(2, 1, 3, 2, 22000, 44000);
+(2, 1, 3, 2, 22000, 44000),
+(3, 2, 4, 2, 25000, 50000),
+(4, 2, 2, 1, 20000, 20000);
 
 -- --------------------------------------------------------
 
@@ -124,11 +127,11 @@ CREATE TABLE `item` (
 
 INSERT INTO `item` (`id`, `item_category_id`, `name`, `image`, `description`, `stock`, `price`, `is_active`, `created_at`, `updated_at`) VALUES
 (1, 1, 'Espresso', '1640398801768.jpeg', 'Single Shot', 30, 12000, 1, '2021-12-13 16:30:20', '2021-12-25 03:15:36'),
-(2, 1, 'Caramel Macchiato (Hot)', '1640398649311.jpeg', 'Single Ristretto with Fresh Milk and Palm Sugar', 22, 20000, 1, '2021-12-13 16:30:49', '2021-12-28 04:35:13'),
+(2, 1, 'Caramel Macchiato (Hot)', '1640398649311.jpeg', 'Single Ristretto with Fresh Milk and Palm Sugar', 21, 20000, 1, '2021-12-13 16:30:49', '2021-12-28 06:56:38'),
 (3, 1, 'Cappucino (Hot)', '1640398850526.jpeg', 'Double Ristretto, Fresh Milk', 21, 22000, 1, '2021-12-13 16:31:19', '2021-12-28 04:35:13'),
-(4, 1, 'Esprecoal (Ice)', '1640398894152.jpeg', 'Freshly Brewed Espresso With Activated Charcoal And Fresh Milk', 36, 25000, 1, '2021-12-13 16:31:42', '2021-12-25 10:23:24'),
-(12, 2, 'Taro Ice', '1640402725769.jpeg', 'Premium Taro Powder With Fresh Milk', 32, 22000, 1, '2021-12-25 03:13:54', '2021-12-25 10:23:24'),
-(13, 2, 'Red Velvet (Hot)', '1640402720531.jpeg', 'Premium Red Velvet with Fresh Milk', 28, 20000, 1, '2021-12-25 03:14:59', '2021-12-25 10:23:24');
+(4, 1, 'Esprecoal (Ice)', '1640398894152.jpeg', 'Freshly Brewed Espresso With Activated Charcoal And Fresh Milk', 34, 25000, 1, '2021-12-13 16:31:42', '2021-12-28 06:56:38'),
+(12, 2, 'Taro Ice', '1640402725769.jpeg', 'Premium Taro Powder With Fresh Milk', 32, 22000, 1, '2021-12-25 03:13:54', '2021-12-28 09:55:05'),
+(13, 2, 'Red Velvet (Hot)', '1640402720531.jpeg', 'Premium Red Velvet with Fresh Milk', 28, 20000, 1, '2021-12-25 03:14:59', '2021-12-28 06:56:53');
 
 -- --------------------------------------------------------
 
@@ -212,19 +215,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT untuk tabel `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `bill_detail`
 --
 ALTER TABLE `bill_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT untuk tabel `item`
